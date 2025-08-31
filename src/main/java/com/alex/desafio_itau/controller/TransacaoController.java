64e824726caa39ae8b79ac76c511665e8027c53f
alex-diasritter <ritter.alex@hotmail.com>
2025-08-31 19:06:11 -0300

    feat/endpoints post, delete e get

---
+++ src/main/java/com/alex/desafio_itau/controller/TransacaoController.java
@@ -0,0 +1,36 @@
+package com.alex.desafio_itau.controller;
+import com.alex.desafio_itau.dto.TransacaoRequestDTO;
+import com.alex.desafio_itau.dto.TransacaoResponseDTO;
+import com.alex.desafio_itau.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TransacaoController {
+
+    @Autowired
+    private TransacaoService service;
+
+    @PostMapping("/transacao")
+    public ResponseEntity<Void> saveTransacao(@RequestBody TransacaoRequestDTO dto) {
+        service.saveTransfers(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/transacoes")
+    public ResponseEntity<String> deleteTransacoes(){
+        service.deleteAll();
+        return ResponseEntity.ok("Todas as informações foram apagadas com sucesso");
+    }
+
+    @GetMapping("/estatisticas")
+    public ResponseEntity<TransacaoResponseDTO> estatistica(){
+        var dto = service.getEstatisticas();
+        return ResponseEntity.ok(dto);
+    }
+}
