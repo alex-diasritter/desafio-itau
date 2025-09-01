fccfce13bc2e544d56927fd778f1516359dadc30
alex-diasritter <ritter.alex@hotmail.com>
2025-09-01 11:49:32 -0300

    feat(TransacaoRequestDTO): adicionando validações no atributo "valor".

--- src/main/java/com/alex/desafio_itau/dto/TransacaoRequestDTO.java
+++ src/main/java/com/alex/desafio_itau/dto/TransacaoRequestDTO.java
@@ -1,11 +1,19 @@
 package com.alex.desafio_itau.dto;
-
+import com.alex.desafio_itau.validation.DataHoraValida;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public class TransacaoRequestDTO {
 
+    @Positive(message = "O valor da transação deve ser positivo.")
+    @DataHoraValida
+    @NotEmpty
     private BigDecimal valor;
+
+    @NotEmpty
     private String dataHora;
+
     private Integer id;
 
     public TransacaoRequestDTO(BigDecimal valor, String dataHora) {
